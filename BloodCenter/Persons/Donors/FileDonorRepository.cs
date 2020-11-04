d4c6eb6ac31698e4ae8544ee6906f2e87ba09633
Tim Nielsen <tim.nielsen@codic.se>
2020-11-04 15:39:16 +0100

    Update added file donor repo

---
+++ BloodCenter/Persons/Donors/FileDonorRepository.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BloodCenter.Persons.Donors
+{
+    public class FileDonorRepository : IDonorRepository
+    {
+
+        private readonly string _appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private readonly string _applicationDirectory = "BloodBank";
+        private readonly string _fileName = "Donors.json";
+        private readonly string _path;
+        private readonly List<Donor> _donors = new List<Donor>();
+
+        public FileDonorRepository()
+        {
+            var directory = Path.Combine(_appData, _applicationDirectory);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            var path = Path.Combine(directory, _fileName);
+            if (!File.Exists(path))
+            {
+                using (File.Create(path)) { }
+            }
+            _path = path;
+            var content = File.ReadAllText(path);
+            var donors = JsonConvert.DeserializeObject<List<Donor>>(content);
+            if (!(donors is null))
+            {
+                _donors = donors;
+            }
+        }
+
+        public void AddDonor(Donor donor)
+        {
+            _donors.Add(donor);
+        }
+
+        public Donor GetDonor(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Donor GetDonor(string socialSecurityNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Donor> GetDonors()
+        {
+            return _donors;
+        }
+
+        public void RemoveDonor(Donor donor)
+        {
+            _donors.Remove(donor);
+        }
+
+        public void Save()
+        {
+            var content = JsonConvert.SerializeObject(_donors);
+            File.WriteAllText(_path, content);
+        }
+    }
+}
---
+++ BloodCenter/Persons/Donors/IDonorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenter.Persons.Donors
+{
+    interface IDonorRepository
+    {
+        Donor GetDonor(Guid id);
+        Donor GetDonor(string socialSecurityNumber);
+        List<Donor> GetDonors();
+        void AddDonor(Donor applicant);
+        void RemoveDonor(Donor applicant);
+        void Save();
+    }
+}
