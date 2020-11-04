0d69011c49441efe6e7f3c87e2790c5ff7c0c727
Tim Nielsen <tim.nielsen@codic.se>
2020-11-04 15:27:35 +0100

    Update FileApplicantRepo using Json

--- BloodCenter/Persons/Applicants/FileApplicantRepository.cs
+++ BloodCenter/Persons/Applicants/FileApplicantRepository.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace BloodCenter.Persons.Applicants
 {
-    class FileApplicantRepository : IApplicantRepository
+    public class FileApplicantRepository : IApplicantRepository
     {
+        private readonly string _appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private readonly string _applicationDirectory = "BloodBank";
+        private  readonly string _fileName = "Applicants.json";
+        private readonly string _path;
+        private readonly List<Applicant> _applicants = new List<Applicant>();
+
+        public FileApplicantRepository()
+        {
+            var directory = Path.Combine(_appData, _applicationDirectory);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            var path = Path.Combine(directory, _fileName);
+            if (!File.Exists(path))
+            {
+                using (File.Create(path)){ }
+            }
+            _path = path;
+            var content = File.ReadAllText(path);
+            var applicants = JsonConvert.DeserializeObject<List<Applicant>>(content);
+            if(!(applicants is null))
+            {
+                _applicants = applicants;
+            }
+        }
+
+        public void AddApplicant(Applicant applicant)
+        {
+            _applicants.Add(applicant);
+        }
 
         public Applicant GetApplicant(Guid id)
         {
-            return null;
+            return _applicants.Single(e => e.Id == id);
         }
 
         public Applicant GetApplicant(string socialSecurityNumber)
@@ -19,7 +54,7 @@ namespace BloodCenter.Persons.Applicants
 
         public List<Applicant> GetApplicants()
         {
-            return null;
+            return _applicants;
         }
 
         public List<Applicant> GetPendingApplicants()
@@ -30,6 +65,17 @@ namespace BloodCenter.Persons.Applicants
         public List<Applicant> GetUnfinishedApplicants()
         {
             return null;
+        }
+
+        public void RemoveApplicant(Applicant applicant)
+        {
+            _applicants.Remove(applicant);
+        }
+
+        public void Save()
+        {
+            var content = JsonConvert.SerializeObject(_applicants);
+            File.WriteAllText(_path, content);
         }
     }
 }
--- BloodCenter/Persons/Applicants/IApplicantRepository.cs
+++ BloodCenter/Persons/Applicants/IApplicantRepository.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 
 namespace BloodCenter.Persons.Applicants
 {
-    interface IApplicantRepository
+    public interface IApplicantRepository
     {
         Applicant GetApplicant(Guid id);
         Applicant GetApplicant(string socialSecurityNumber);
         List<Applicant> GetApplicants();
         List<Applicant> GetPendingApplicants();
         List<Applicant> GetUnfinishedApplicants();
+        void AddApplicant(Applicant applicant);
+        void RemoveApplicant(Applicant applicant);
+        void Save();
     }
 }
