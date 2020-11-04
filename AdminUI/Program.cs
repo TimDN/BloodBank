0d9f901047a727b539a8cc8b44eb7faeb0635402
Tim Nielsen <tim.nielsen@codic.se>
2020-11-04 15:54:29 +0100

    Update admin UI

---
+++ AdminUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using BloodCenter.Blood;
+using BloodCenter.Persons;
+using BloodCenter.Persons.Applicants;
+using System;
+
+namespace AdminUI
+{
+    class Program
+    {
+        private static IApplicantRepository _applicantRepository;
+
+        static void Main(string[] args)
+        {
+            _applicantRepository = new FileApplicantRepository();
+
+            try
+            {
+
+            }
+            finally
+            {
+                _applicantRepository.Save();
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void CreateApplicant()
+        {
+            var applicant = new Applicant(Guid.NewGuid(), true, BloodType.ABn);
+            _applicantRepository.AddApplicant(applicant);
+        }
+    }
+}
